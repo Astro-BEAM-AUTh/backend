9c3f2283407a5aa44df5ed884cfc0e06478c1a08
Hercules Konsoulas <dyka3773@gmail.com>
2026-05-06 21:13:30 +0200

    chore: Migrate project to supabase and thus reinstantiate the initialization scripts just to be safe

--- migrations/versions/sql/20260506_0001_downgrade.sql
+++ migrations/versions/sql/20260506_0001_downgrade.sql
@@ -1,8 +1,8 @@
--- Downgrade SQL for revision 20260505_0001
+-- Downgrade SQL for revision 20260506_0001
 
 BEGIN;
 
--- Running downgrade 20260505_0001 -> 
+-- Running downgrade 20260506_0001 -> 
 
 DROP INDEX ix_observations_observation_id;
 
@@ -18,7 +18,7 @@ DROP TABLE users;
 
 DROP TYPE observationstatus;
 
-DELETE FROM alembic_version WHERE alembic_version.version_num = '20260505_0001';
+DELETE FROM alembic_version WHERE alembic_version.version_num = '20260506_0001';
 
 DROP TABLE alembic_version;
 
